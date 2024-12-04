f1b5a7ce03fc78892cfd275db15ac2018d1dba27
AbdulazizJastanieh <asalehjastanieh@stu.kau.edu.sa>
2024-12-04 17:53:43 +0300

    d
    d

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const AboutUs = () => {
+    return (
+        <div id="about-us">
+            <h1 >About Us</h1>
+            <p>
+                Welcome to our music search react application. this application uses Itunes api to search for music 
+            </p>
+            <h2>How It Works</h2>
+            <p>
+                We fetches music  from the Itunes API, which provides a large database of songs and artists.
+                You can search for songs based on the song name or the artist name. just enter the name of the song or the artist and the app will show you results from Itunes.
+            </p>
+
+            
+
+        </div>
+    );
+};
+
+export default AboutUs;
